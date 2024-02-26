fea1863255c0ec27ded67f3442f6a45627c2d684
Mohammed Usama <usamabhakrani@gmail.com>
2024-02-26 19:09:19 +0500

    Code Splitting routes and controllers

--- starter/app.js
+++ starter/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const taskRouter = require("./routes/tasks");
+
+// MiddleWare
+app.use(express.static("./public"));
+app.use(express.json());
+
+// Routes
+app.use("/api/v1/tasks", taskRouter);
 
 app.get("/", (req, res) => res.send("Hello"));
+
+// app.get('/api/v1/tasks')
+// app.post('/api/v1/tasks')
+// app.get('/api/v1/tasks/:id')
+// app.put('/api/v1/tasks/:id')
+// app.delete('/api/v1/tasks/:id')
 
 app.listen(port, () => console.log("Server running on port", port));
---
+++ starter/controllers/tasks.js
@@ -0,0 +1,7 @@
+const getAllTasks = (req, res) => {
+  res.send("All Items From the File");
+};
+
+module.exports = {
+  getAllTasks,
+};
