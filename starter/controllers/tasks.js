2b10379f38fd9a835bb352906e689d467f57abe7
UsamaBhakrani <usamabhakrani@gmail.com>
2024-02-26 22:09:48 +0500

    api functions and controllers with routes

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,7 +1,25 @@
+
+
 const getAllTasks = (req, res) => {
   res.send("All Items From the File");
+};
+const createTask = (req, res) => {
+  res.send("Create Task");
+};
+const getTask = (req, res) => {
+  res.send("Get Task");
+};
+const updateTask = (req, res) => {
+  res.send("Update Task");
+};
+const deleteTask = (req, res) => {
+  res.send("Delete Task");
 };
 
 module.exports = {
   getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 };
